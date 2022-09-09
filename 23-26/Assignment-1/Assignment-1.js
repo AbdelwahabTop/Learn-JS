dbcd7f4f43d53e77944a42eb95501771c5333f0b
Saitama <mramged936@gmail.com>
2022-09-09 17:56:01 +0200

    Week 3 & 4

---
+++ 23-26/Assignment-1/Assignment-1.js
@@ -0,0 +1,17 @@
+/*
+// Examples
+console.log(100_000); // 100000
+console.log(100000); // 100000
+console.log(5e4 + 5e4); // 100000
+*/
+// Your Solutions
+console.log(100000.000); // 100000
+console.log(100000..toString()); // 100000
+console.log(parseInt("100000 Elzero")); // 100000
+console.log(Math.round(99999.9)); // 100000
+console.log(Math.ceil(99999.1)); // 100000
+console.log(Math.floor(100000.9)); // 100000
+console.log(Math.trunc(100000.9)); // 100000
+console.log(10 ** 5); // 100000
+console.log(Math.pow(10, 5)); // 100000
+console.log(Math.max(100000, 5, -10)); // 100000
---
+++ 23-26/Assignment-4/Assignment-4.js
@@ -0,0 +1,5 @@
+let myVar = "100.56789 Views";
+
+console.log(Number.parseInt(myVar)); // 100
+console.log(typeof(Number.parseInt(myVar)))
+console.log(Number.parseFloat(myVar).toFixed(2)); // 100.57
