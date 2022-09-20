db916693dcbd0ebacde88763ca4258a9a6ed2698
Saitama <mramged936@gmail.com>
2022-09-20 23:41:08 +0200

    Week 15

---
+++ 79 - 85/Assignment - 2/main.js
@@ -0,0 +1,19 @@
+// Method One
+// Create Your Object Here
+let objMethodOne = {};
+console.log(objMethodOne.property); // "Method One"
+
+// Method Two
+// Create Your Object Here
+let objMethodTwo = new Object({});
+console.log(objMethodTwo.property); // "Method Two"
+
+// Method Three
+// Create Your Object Here
+let objMethodThree = Object.create({});
+console.log(objMethodThree.property); // "Method Three"
+
+// Method Four
+// Create Your Object Here
+let objMethodFour = Object.create(objMethodOne);
+console.log(objMethodFour.property); // "Method Four"
