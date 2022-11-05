b80c6fe3e9c46ba79527bb348e5c7e4bf621692b
Saitama <mramged936@gmail.com>
2022-11-05 19:54:46 +0200

    Challenge 3

---
+++ Jonas Schmedtmann/16 Promises,/Challenge 3/script.js
@@ -0,0 +1,88 @@
+"use strict";
+
+const wait = function (seconds) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, seconds * 1000);
+  });
+};
+
+const imgContainer = document.querySelector(".images");
+
+const createImage = function (imgPath) {
+  return new Promise(function (resolve, reject) {
+    const img = document.createElement("img");
+    img.src = imgPath;
+
+    img.addEventListener("load", function () {
+      imgContainer.append(img);
+      resolve(img);
+    });
+
+    img.addEventListener("error", function () {
+      reject(new Error("Image not found"));
+    });
+  });
+};
+
+
+const short = async function (url, t) {
+  let img = await createImage(url);
+  await wait(t);
+  img.style.display = "none";
+};
+/*
+const loadNPause = async function () {
+  try {
+    await short("./img/img-1.jpg", 2);
+
+    await short("./img/img-2.jpg", 2);
+
+    await short("./img/img-3.jpg", 2);
+  } catch (err){
+    console.error(err);
+  }
+};
+loadNPause();*/
+
+const loadAll = async function(imgArr) {
+    try{
+        const imgs = imgArr.map(async img => await createImage(img))
+
+        console.log(imgs);
+    
+        const imgsEl = await Promise.all(imgs)
+        console.log(imgsEl);
+        imgsEl.forEach(img => img.classList.add('parallel'))
+
+    } catch (err) {
+        console.log(err);
+    }
+
+}
+
+loadAll(['img/img-1.jpg', 'img/img-2.jpg', 'img/img-3.jpg']);
+
+
+
+/*
+const loadNPause = async function () {
+  try {
+    let img = await createImage("./img/img-1.jpg");
+    await wait(2);
+    img.style.display = 'none';
+
+    let img  = await createImage("./img/img-2.jpg");
+    currentImg = two;
+    await wait(2);
+    img.style.display = 'none';
+
+    let img  = await createImage("./img/img-3.jpg");
+    currentImg = three;
+    await wait(2);
+    img.style.display = 'none';
+  } catch (err){
+    console.log(err);
+  }
+};
+loadNPause();
+*/
